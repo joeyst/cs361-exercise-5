6a8f5725cc614137fd4fe1c9094d723ac72453a4
joeyst <joeystenbeck@gmail.com>
2022-10-24 16:02:07 -0700

    Remove dead function.

--- ex5.rb
+++ ex5.rb
@@ -24,13 +24,6 @@ class LaunchDiscussionWorkflow
       end
     end
   end
-
-  def generate_participant_users_from_email_string
-    return if @participants_email_string.blank?
-    @participants_email_string.split.uniq.map do |email_address|
-      User.create(email: email_address.downcase, password: Devise.friendly_token)
-    end
-  end
   
   private: 
   def get_list_of_emails(str)
